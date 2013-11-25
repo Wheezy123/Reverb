96541e1e909fad32288d461165e114c0491c66e8
Jerome Johnson <JohnsonJeromeC@gmail.com>
2013-11-24 19:25:30 -0600

    initialized project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reverb::Application.config.secret_key_base = 'edbc3e2fcfbf7491a5b53f9563d971d8e8ddabe2956cfd7e6e710d1639a47e523a8874b782a2fbda7b22583b236a9f68b003aed5a4f55b2c5ce59a93b712d83d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reverb::Application.config.session_store :cookie_store, key: '_Reverb_session'
