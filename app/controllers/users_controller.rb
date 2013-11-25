902e58d65363c3a15e9a374e9f8dd2165e11a1a0
Jerome Johnson <JohnsonJeromeC@gmail.com>
2013-11-24 20:46:23 -0600

    added Ladies First sorting table and added sorting db query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @ladies = User.all(:order =>'gender, last_name')
+   
   end
 
   def import
