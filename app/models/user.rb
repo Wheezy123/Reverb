df6d3cac4cee04369c8d5af1f4cf573e3af33be7
Jerome Johnson <JohnsonJeromeC@gmail.com>
2013-11-24 20:10:35 -0600

    can upload csv files into database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+  def self.import(file)
+    CSV.open(file.path, 'r',{:headers => true}).each do |row|
+      User.create(:last_name => row[0], :first_name => row[1], :gender => row[2],
+        :favorite_color => row[3], :date_of_birth => row[4])
+    end
+  end
 end
