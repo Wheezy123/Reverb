c597cedf9ee8389fe456de333a08bd1c5709c539
Jerome Johnson <JohnsonJeromeC@gmail.com>
2013-11-24 20:40:59 -0600

    can now upload CSV, PSV, and TSV files successfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,20 @@ class User < ActiveRecord::Base
   def self.import(file)
     csv = File.open(file.path, 'rb')
     contents = file.read
-    if contents.include?("\t")
+    if contents.include?("|")
+      CSV.open(file.path, 'r', { :col_sep => '|', :headers => true}).each do |row|
+        User.create(:last_name => row[0], :first_name => row[1], :gender => row[2],
+          :date_of_birth => row[3], :favorite_color => row[4])
+      end
+    elsif contents.include?("\t")
       CSV.open(file.path, 'r', { :col_sep => "\t", :headers => true}).each do |row|
         User.create(:last_name => row[0], :first_name => row[1], :gender => row[2],
-          :favorite_color => row[3], :date_of_birth => row[4])
+          :date_of_birth => row[3], :favorite_color => row[4])
       end
     else
       CSV.open(file.path, 'r',{:headers => true}).each do |row|
         User.create(:last_name => row[0], :first_name => row[1], :gender => row[2],
-          :favorite_color => row[3], :date_of_birth => row[4])
+          :date_of_birth => row[3], :favorite_color => row[4])
       end
     end
   end
